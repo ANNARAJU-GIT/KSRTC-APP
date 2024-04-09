5084eb53c6f8502b5ffb07b1b9556667cd95b584
Annaraju <annaraju311@gmail.com>
2024-04-09 23:16:09 +0530

    KSRTC-APP finished

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import DeleteBus from './components/DeleteBus';
 import SearchBus from './components/SearchBus';
+import AddBus from './components/AddBus';
+import ViewAll from './components/ViewAll';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-
+     <Navbar/>
      <SearchBus/>
+     <DeleteBus/>
+     <AddBus/>
+     <ViewAll/>
+
 
 
     </div>
