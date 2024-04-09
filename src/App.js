17273defa61f36da4d16cc342124c67aa45b39fd
Annaraju <annaraju311@gmail.com>
2024-04-09 23:11:24 +0530

    searchbus finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import DeleteBus from './components/DeleteBus';
+import SearchBus from './components/SearchBus';
 
 function App() {
   return (
     <div>
 
-     
+     <SearchBus/>
+
 
     </div>
   );
