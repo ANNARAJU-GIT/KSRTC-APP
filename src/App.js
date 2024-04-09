4dd06ee31282207bf110b4dd63d5e3b60ade637e
Annaraju <annaraju311@gmail.com>
2024-04-09 21:31:43 +0530

    Navbar ceated

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import AddBus from './components/AddBus';
 import DeleteBus from './components/DeleteBus';
 import ViewAll from './components/ViewAll';
+import SearchBus from './components/SearchBus';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <ViewAll/>
+      
+
     </div>
   );
 }
--- src/components/SearchBus.jsx
+++ src/components/SearchBus.jsx
@@ -6,6 +6,7 @@ const SearchBus = () => {
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <p><center><b><h1>SEARCH FOR BUSES</h1></b></center></p>
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label className="form-label">Bus Number</label>
