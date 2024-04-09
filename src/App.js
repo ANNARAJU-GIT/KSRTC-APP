9c4dcf943b3c5b2213db73fa80c0327d6f362776
Annaraju <annaraju311@gmail.com>
2024-04-09 23:08:45 +0530

    delete bus finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AddBus from './components/AddBus';
-import SearchBus from './components/SearchBus';
-import DeleteBus from './components/DeleteBus';
-import ViewAll from './components/ViewAll';
+
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<AddBus/>} />
-      <Route path='/search' element={<SearchBus/>}/>
-      <Route path='/delete' element={<DeleteBus/>}/>
-      <Route path='/view' element={<ViewAll/>}/>
-    </Routes>
-  </BrowserRouter>
+    <div>
+
+     
+
+    </div>
   );
 }
 
