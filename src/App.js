2c90d5b0039dc8122ad06c93d44f9eab10fbf179
Annaraju <annaraju311@gmail.com>
2024-04-09 22:24:06 +0530

    VIEW ALL FINISHED

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddBus from './components/AddBus';
+import SearchBus from './components/SearchBus';
 import DeleteBus from './components/DeleteBus';
 import ViewAll from './components/ViewAll';
-import SearchBus from './components/SearchBus';
-import Navbar from './components/Navbar';
-
 function App() {
   return (
-    <div>
-      
-
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddBus/>} />
+      <Route path='/search' element={<SearchBus/>}/>
+      <Route path='/delete' element={<DeleteBus/>}/>
+      <Route path='/view' element={<ViewAll/>}/>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,16 +12,16 @@ const Navbar = () => {
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="">HOME</a>
+          <a class="nav-link active" aria-current="page" href="/">ADD</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="/search">SEARCH</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="/delete">DELETE</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="/view">VIEW ALL</a>
         </li>
       </ul>
     </div>
